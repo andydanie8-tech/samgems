53c953fb2660fea12528493a1ec90d735fae0fa7
andydanie8-tech <andydanie8@gmail.com>
2025-11-17 21:44:53 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gold: "#D4AF37",
+        navy: "#0A0F1A"
+      },
+      fontFamily: {
+        serif: ["Playfair Display", "serif"],
+        sans: ["Inter", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
